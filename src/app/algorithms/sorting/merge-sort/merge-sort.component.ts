8a3d19d5a32054e6314078c25f15e2ab8a9c1cfc
Vamsi Krishna Kodimela <55780006+vamsi-krishna-kodimela@users.noreply.github.com>
2023-12-26 17:14:47 +0000

    Refactor merge-sort component to include sorting functionality

--- src/app/algorithms/sorting/merge-sort/merge-sort.component.ts
+++ src/app/algorithms/sorting/merge-sort/merge-sort.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-merge-sort',
   standalone: true,
   imports: [],
   templateUrl: './merge-sort.component.html',
-  styleUrl: './merge-sort.component.scss'
+  styleUrl: './merge-sort.component.scss',
 })
-export class MergeSortComponent {
+export class MergeSortComponent implements OnInit {
+  ngOnInit(): void {
+    this.sort();
+  }
 
+  sort() {
+    let numbers: number[] = [15, 234, 2, 0, 23, 76, 45, 21];
+    for (let i = 1; i < numbers.length; i++) {
+      let temp: number = numbers[i];
+      let j = i;
+
+      while (numbers[j - 1] > temp) {
+        numbers[j] = numbers[j - 1];
+        j--;
+      }
+
+      numbers[j] = temp;
+    }
+    console.log(numbers);
+  }
 }
